cfa65a0cc2330bc94039d1956f5cae50fba3aa11
old7evennn <167318055+old7evennn@users.noreply.github.com>
2024-06-13 12:55:51 +0300

    change URL

--- src/constans.ts
+++ src/constans.ts
@@ -1,4 +1,1 @@
-export const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.NODE_ENV
-    : "https://express-server-production-1871.up.railway.app"
+export const BASE_URL = "https://express-server-production-1871.up.railway.app"
